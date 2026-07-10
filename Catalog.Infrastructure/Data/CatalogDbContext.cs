4b00277df9113c8372e47e8e620f925a9d5d1bfe
Carlos Santana <codebycs@gmail.com>
2026-07-10 19:35:26 +0100

    Initial structure: 4 APIs + Shared.Contracts_2

---
+++ Catalog.Infrastructure/Data/CatalogDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FacilitiesCatalog.API
+{
+    public class CatalogDbContext : DbContext
+    {
+        public CatalogDbContext(DbContextOptions<CatalogDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasDefaultSchema("catalog");
+        
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ Shared.Contracts/Booking/BookCourtRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Contracts.Booking
+{
+    internal class BookCourtRequest
+    {
+        public Guid CourtId { get; set; }
+        public DateTime Date { get; set; }
+        public TimeSpan StartTime { get; set; }
+        public TimeSpan EndTime { get; set; }
+        public List<Guid> ExtraEquipmentIds { get; set; } = new();
+    }
+}
