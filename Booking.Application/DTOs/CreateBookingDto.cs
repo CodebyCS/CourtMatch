76fe3a165367cab5260568248a13edbfe0232c5f
emersonr02 <254676587+emersonr02@users.noreply.github.com>
2026-07-10 22:57:02 +0100

    Initial Application

---
+++ Booking.Application/DTOs/CreateBookingDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booking.Application.DTOs
+{
+    public record CreateBookingDto(
+        Guid CourtId,
+        Guid HostPlayerId,
+        DateTime Schedule,
+        decimal TotalPrice
+        );
+}
