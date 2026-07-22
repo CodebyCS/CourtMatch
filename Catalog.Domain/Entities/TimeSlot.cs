0e8303498fc1a97723ead7174c8f12b419ad8ab6
Vi-XavierSantos <xavysantos1@gmail.com>
2026-07-22 23:03:50 +0100

    feat: Adicionado as Entities e Repositories

---
+++ Catalog.Domain/Entities/TimeSlot.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalog.Domain.Entities
+{
+    public class TimeSlot
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public TimeSpan StartTime { get; set; }
+
+        public TimeSpan EndTime { get; set; }
+
+    }
+}
