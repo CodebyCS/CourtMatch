2e112e98bd768491a6d2924fead08d0753d3faa5
Carlos Santana <codebycs@gmail.com>
2026-07-24 23:42:31 +0100

    feat(shared): add base exception hierarchy

---
+++ src/BuildingBlocks/Shared/Shared.Contracts/Exceptions/BadRequestException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Contracts.Exceptions
+{
+    /// <summary>
+    /// Exception thrown when a bad request (HTTP 400) occurs within the application.
+    /// </summary>
+    /// <seealso cref="Shared.Contracts.Exceptions.AppException" />
+    public class BadRequestException : AppException
+    {
+        public BadRequestException(string message) : base(message, 400) { }
+    }
+}
---
+++ src/BuildingBlocks/Shared/Shared.Contracts/Exceptions/UnauthorizedAppException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Contracts.Exceptions
+{
+    /// <summary>
+    /// Exception thrown when an unauthorized access (HTTP 401) error occurs within the application.
+    /// </summary>
+    /// <seealso cref="Shared.Contracts.Exceptions.AppException" />
+    public class UnauthorizedAppException : AppException
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UnauthorizedAppException"/> class.
+        /// </summary>
+        /// <param name="message">The message that describes the error.</param>
+        public UnauthorizedAppException(string message) : base(message, 401) { }
+    }
+}
