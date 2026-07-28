91910d7e4856a288ed217798c980fda93addda39
Vi-XavierSantos <xavysantos1@gmail.com>
2026-07-28 21:52:55 +0100

    fix: Controllers

--- src/Modules/Catalog/Catalog.Application/DTOs/EquipmentDto.cs
+++ src/Modules/Catalog/Catalog.Application/DTOs/EquipmentDto.cs
@@ -8,7 +8,7 @@ namespace Catalog.Application.DTOs
 {
     public class EquipmentDto
     {
-        public Guid Id { get; set; }
+        public Guid Id { get; set; } = Guid.NewGuid();
         public string Name { get; set; } = string.Empty;
         public string Category { get; set; } = string.Empty;
         public int Stock { get; set; }
